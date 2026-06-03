427916a43ab5ab037f2a925d37b61c4124f6d24d
Deepti-lodhi-2003 <lodhideepti39@gmail.com>
2026-06-03 15:51:24 +0530

    add vercel frontend URL to CORS origins

--- src/app.js
+++ src/app.js
@@ -14,7 +14,7 @@ app.use(helmet());
 
 // CORS
 app.use(cors({
-    origin: ['http://localhost:5173','http://localhost:5174'],
+    origin: ['http://localhost:5173','http://localhost:5174', 'https://utkarsh-portal-frontend-e11s.vercel.app'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
     allowedHeaders: ['Content-Type', 'Authorization']
